294970ab3e7cbd420ba68430fa41109c72a0e6ef
Alexander Malyshev <san-100@bk.ru>
2019-11-04 22:36:55 +0300

    Пользовательские классы

---
+++ src/main/PersonalData.kt
@@ -0,0 +1,14 @@
+package main
+
+class PersonalData(
+    val firstName: String,
+    val lastName: String,
+    val middleName: String,
+    val age: Int,
+    val gender: Char,
+    val dateOfBirth: String,
+    val placeOfBirth: String,
+    val placeOfResidence: PlaceOfResidence
+) {
+
+}
---
+++ src/main/PlaceOfResidence.kt
@@ -0,0 +1,11 @@
+package main
+
+class PlaceOfResidence(
+    val zip: String,
+    val country: String,
+    val state: String,
+    val city: String,
+    val street: String,
+    val building: String,
+    val apartment: String) {
+}
